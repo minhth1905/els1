607bcef9dc9ca64b861e616ba123b97faf463e03
hieptm96 <hiep1996tb@gmail.com>
2016-07-02 15:41:52 +0700

    304-update-categories

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,3 @@
+setTimeout(function () {
+  document.getElementsByClassName("alert-message")[0].innerHTML = "";
+}, 3000);
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,9 +10,26 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
+      flash[:success] = t("categories.success_create")
       redirect_to @category
     else
+      flash.now[:danger] = t("categories.fail_create")
       render "new"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:success] = t("categories.success_update")
+      redirect_to @category
+    else
+      flash.now[:danger] = t("categories.fail_update")
+      render "edit"
     end
   end
 
