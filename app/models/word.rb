3dce2611eeefe9f64d2bcaf47c3b892db60a4a64
hieptm96 <hiep1996tb@gmail.com>
2016-07-06 10:07:44 +0700

    311-filter-word-in-category

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,9 @@
 class Word < ActiveRecord::Base
   belongs_to :category
-
   has_many :answers
+
+  scope :learned, ->(current_user_id){
+    where("words.id in (select word_id from results where user_id = ?)", current_user_id)}
+  scope :notlearned, ->(current_user_id){
+    where("words.id not in (select word_id from results where user_id = ?)", current_user_id)}
 end
